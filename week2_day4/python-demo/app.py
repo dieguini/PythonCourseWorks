7974cc37760ddd39e51d2aea24efb2bb1299453b
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-09-17 12:56:31 -0400

    feat: python-demo logic implemented for creating a file base on API

---
+++ week2_day4/python-demo/app.py
@@ -0,0 +1,22 @@
+"""
+    Entry point for my application
+"""
+import utils
+from rest_client import get_demo_api_data
+
+from files_helper import write_data_in_file
+from files_helper import create_output_folder
+
+def main():
+    status_code, response_data, extension = get_demo_api_data()
+
+    if status_code != 200:
+        print("Can't continue, API not working")
+        return
+
+    file_name = utils.get_filename(extension=extension)
+
+    create_output_folder()
+    write_data_in_file(response_data, file_name)
+
+main()
